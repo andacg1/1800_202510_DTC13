38d392ef0534e0526e37035e5826b499df868dad
Deniz Gunay <andacg1@hotmail.com>
2025-02-13 14:04:46 -0800

    Polish bottom-navbar.ts

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@ import "@shoelace-style/shoelace/dist/components/button/button.js";
 import "@shoelace-style/shoelace/dist/components/icon/icon.js";
 import "@shoelace-style/shoelace/dist/components/input/input.js";
 import "@shoelace-style/shoelace/dist/components/rating/rating.js";
+import "@shoelace-style/shoelace/dist/components/tab/tab.js";
 // import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
 
--- src/components/bottom-navbar.ts
+++ src/components/bottom-navbar.ts
@@ -13,10 +13,25 @@ export class BottomNavbar extends withTwind(LitElement) {
   name = "Somebody";
 
   render() {
-    return html`<div class="fixed bottom-0 left-0 w-full h-32  bg-amber-300">
+    return html`<div
+      class="fixed bottom-0 left-0 w-full h-32 p-2  bg-amber-300"
+    >
       <sl-button variant="primary">Primary</sl-button>
       <sl-button variant="default">Default</sl-button>
       <sl-button variant="primary" outline>Primary</sl-button>
+      <!--      <sl-tab-group>-->
+      <!--        <sl-tab slot="nav" panel="general">General</sl-tab>-->
+      <!--        <sl-tab slot="nav" panel="custom">Custom</sl-tab>-->
+      <!--        <sl-tab slot="nav" panel="advanced">Advanced</sl-tab>-->
+
+      <!--        <sl-tab-panel name="general"-->
+      <!--          >This is the general tab panel.</sl-tab-panel-->
+      <!--        >-->
+      <!--        <sl-tab-panel name="custom">This is the custom tab panel.</sl-tab-panel>-->
+      <!--        <sl-tab-panel name="advanced"-->
+      <!--          >This is the advanced tab panel.</sl-tab-panel-->
+      <!--        >-->
+      <!--      </sl-tab-group>-->
     </div>`;
   }
 }
