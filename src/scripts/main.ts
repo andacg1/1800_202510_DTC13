e2d05ce39a4dcb0444c32a85c648b9f3d22b20b9
Deniz Gunay <andacg1@hotmail.com>
2025-03-03 09:16:39 -0800

    add create.html

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,11 +1,5 @@
 import "cally";
-import { library, icon } from "@fortawesome/fontawesome-svg-core";
-import { faCamera } from "@fortawesome/free-solid-svg-icons";
 import { hydrate } from "./hydrate";
-
-library.add(faCamera);
-
-const camera = icon({ prefix: "fas", iconName: "camera" });
 
 // Set the base path to the folder you copied Shoelace's assets to
 console.log("SCRIPT");
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   // root: "src",
   build: {
     rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, "main.html"),
+        login: resolve(__dirname, "login.html"),
+        create: resolve(__dirname, "create.html"),
+      },
       plugins: [],
     },
   },
   optimizeDeps: {
-    entries: "src/**/*{.html,.css,.js}",
+    entries: ["/*.html", "src/**/*{.html,.css,.js}"],
   },
   plugins: [tailwindcss()],
 });
