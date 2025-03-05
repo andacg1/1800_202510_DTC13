22cd18826e3a979f4aa5844fe9735684ff08a628
Deniz Gunay <andacg1@hotmail.com>
2025-03-05 11:22:05 -0800

    upgraded firebase to modular API

--- window.d.ts
+++ window.d.ts
@@ -1,12 +1,20 @@
+import type { FirebaseApp } from "firebase/app";
+import type { Firestore } from "firebase/firestore";
+import type { Auth } from "firebase/auth";
+
 type App = {
   state: {
     create: {
       step: number;
     };
   };
+  db: Firestore;
+  app: FirebaseApp;
+  auth: Auth;
 };
 
-interface Window {
-  App: App;
+declare global {
+  interface Window {
+    App: App;
+  }
 }
-
